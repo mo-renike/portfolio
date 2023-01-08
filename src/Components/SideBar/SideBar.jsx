2dcb6d60b912d068a71b98fecccfa2a8a4e6de26
mo-renike <herroyalpianist@gmail.com>
2023-01-08 14:13:31 +0100

    FIX/ Remove call section of the sidebar

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import "./SideBar.scss";
 // import react-icons
-import { FaTwitter, FaLinkedin, FaEnvelope, FaPhone, FaWhatsapp } from "react-icons/fa";
+import { FaTwitter, FaLinkedin, FaEnvelope, FaWhatsapp } from "react-icons/fa";
 
 
 const SideBar = () => {
     return (
         <div className="sidebar">
-            <a href="tel:+2348099772916"><FaPhone /></a>
+            {/* <a href="tel:+2348099772916"><FaPhone /></a> */}
             <a href="https://twitter.com/Mo_renike_"
             ><FaTwitter /></a>
             <a href="mailto:herroyalpianist@gmail.com"><FaEnvelope /></a>
             <a href="https://www.linkedin.com/in/morenike-oyewole/"
             ><FaLinkedin /></a>
             <a href="https://wa.link/tlqzgl"><FaWhatsapp /></a>
+            <span className="line"></span>
         </div>
     )
 }
