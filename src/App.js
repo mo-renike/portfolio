decb762b2d15ec7f2d4b2c54b8b7a1455593f6ff
mo-renike <herroyalpianist@gmail.com>
2023-01-25 01:15:33 +0100

    BUG/ hide cursor on mobile and touch screen devices

--- src/App.js
+++ src/App.js
@@ -49,6 +49,18 @@ const App = () => {
   const links = document.querySelectorAll("a");
   const btns = document.querySelectorAll("button");
 
+  // hide cursor on mobile devices
+  if (window.innerWidth < 768) {
+    cursor.style.display = "none";
+    cursor1.style.display = "none";
+  }
+
+  // hide cursor on touch devices
+  window.addEventListener("touchstart", () => {
+    cursor.style.display = "none";
+    cursor1.style.display = "none";
+  });
+
   links.forEach((link) => {
     link.addEventListener("mouseover", () => {
       cursor.classList.add("link-grow");
